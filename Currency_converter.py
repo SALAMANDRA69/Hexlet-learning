e693600a9a07057fb6bc3b7cb844009f35bf4987
SERGEY <105315873+SALAMANDRA69@users.noreply.github.com>
2023-09-23 15:56:26 +1000

    Update_1.1 Currency_converter.py

--- Currency_converter.py
+++ Currency_converter.py
@@ -9,14 +9,14 @@ def getting_currency_quotes():
 
 CURRENCIES = getting_currency_quotes()
 
-def checking_availability_currency(CURRENCIES):
+def checking_availability_currency(currencies):
     while True:
-        current_currency = input("Введите исходную валюту: ")
-        if current_currency in CURRENCIES['data']:
+        current_currency = input('Введите исходную валюту: ')
+        if current_currency in currencies['data']:
             break
     while True:
-        result_currency = input("Введите результирующую валюту: ")
-        if result_currency in CURRENCIES['data']:
+        result_currency = input('Введите результирующую валюту: ')
+        if result_currency in currencies['data']:
             break
     return current_currency, result_currency
 
@@ -26,23 +26,24 @@ def convert(amount, from_currency, to_currency, currencies):
     coefficient = to_value / from_value
     return round(amount * coefficient, 2)
 
-def main(CURRENCIES):
+def main(currencies):
     status_actions = None
     print('''ДОБРО ПОЖАЛОВАТЬ В КОНВЕРТ ВАЛЮТ!
+
 1 - Список валют с ценами
 2 - Конверт валют
 0 - Выход''')
-    while status_actions != 0:
-        status_actions = int(input('\nНажмите для просмотра: '))
-        if status_actions == 1:
+    while status_actions != '0':
+        status_actions = input('\nНажмите для просмотра: ')
+        if status_actions == '1':
             print('\nСПИСОК ВАЛЮТ С ЦЕНАМИ\n')       
-            for i in CURRENCIES['data']:
+            for i in currencies['data']:
                 print(f'{i} = {round(CURRENCIES["data"][i], 2)}')       
-        elif status_actions == 2:
+        elif status_actions == '2':
             print('\nКОНВЕРТ ВАЛЮТ\n')
-            currency = checking_availability_currency(CURRENCIES)
+            currency = checking_availability_currency(currencies)
             amount = input("Введите количество: ")
-            result = convert(float(amount), currency[0], currency[1], CURRENCIES['data'])
+            result = convert(float(amount), currency[0], currency[1], currencies['data'])
             print(f'\n{amount} {currency[0]} = {result} {currency[1]}')
     print('\nДО НОВЫХ ВСТРЕЧ!')
 
